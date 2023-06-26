ee51d7fe9ed3364662aae28990fc9c4c1fb3da04
Zeshan Shahid <z.shahid@hotmail.de>
2023-06-26 18:05:56 +0200

    create an input to add a Task, create a type for Task, implement method to handle input-change, implement function to add a task by clicking on a add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
+import {ChangeEvent, useState} from "react";
 
 
 function App() {
+    const task1: Task = {id:1,description: "tidy up", status: "Open"}
+    const [taskList, setTaskList] = useState<Task[]>([task1])
+    const [description, setDescription] = useState("")
 
 
-  return (
-    <>
+    function handleInput(event: ChangeEvent<HTMLInputElement>) {
+        setDescription(event.target.value)
+    }
 
-    </>
-  )
+    function addTask() {
+        setTaskList([...taskList, {
+            id: taskList.length + 1,
+            description: description,
+            status: "OPEN"
+        }])
+    }
+
+    return (
+        <>
+
+            <h1>Add Task</h1>
+            <input onInput={handleInput}/>
+            <button onClick={addTask}>Add</button>
+
+            {taskList.map(task => <div key={task.id}>
+                <h3>{task.description}</h3>
+                <p>{task.status}</p>
+            </div>)}
+        </>
+    )
 }
 
 export default App
---
+++ src/Task.ts
@@ -0,0 +1,5 @@
+type Task = {
+    id:number
+    description: string
+    status: string
+}
